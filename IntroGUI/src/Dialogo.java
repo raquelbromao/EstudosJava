959d4a431755244da95fbd8aa4d0fc5178086aa8
raquelbromao <raquelbromao@gmail.com>
2016-07-01 00:09:54 -0300

    comitted estudos iniciais em GUI em java no Eclipse IDE

---
+++ IntroGUI/src/Dialogo.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;
+
+//Pacote javax.swing possui muitas classes que auxiliam na criação de interfaces gráficas
+
+/**
+ * @author Romao
+ * @date 30/06/16
+ */
+
+public class Dialogo {
+	
+	public static void main(String args[]) {
+		// EXIBE diálogo com mensagem
+		// Primeiro argumento: posição da caixa na tela, null centraliza
+		// Segundo argumento: mensagem em si
+		JOptionPane.showMessageDialog(null, "Welcome \nTo Java");
+
+		// EXIBE caixa de diálogo com inserção de entrada pelo usuário
+		String noome = JOptionPane.showInputDialog("Qual seu noome?");
+
+		// FORMATA a string adicionando o noome do usuário
+		String mensagem = String.format("Bem-vindo, %s, a Programacao Java", noome);
+
+		// EXIBE a mensagem com o noome do usuário
+		JOptionPane.showMessageDialog(null, mensagem);
+	}
+}
